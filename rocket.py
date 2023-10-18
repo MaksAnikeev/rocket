ac925aa77995a4d8b3abdeeea826c50c16ead284
MaksAnikeev <anikeev.maks@rambler.ru>
2023-10-18 07:23:16 +0300

    Initial commit

---
+++ rocket.py
@@ -0,0 +1,37 @@
+import time
+import asyncio
+import curses
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+def draw(canvas):
+    curses.curs_set(False)
+    canvas.border()
+    coroutines = []
+    for i in range(5):
+        row, column = (2, 10+i*2)
+        coroutine = blink(canvas, row, column)
+        coroutines.append(coroutine)
+    while True:
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
+        canvas.refresh()
+        time.sleep(1)
+        if not len(coroutines):
+            break
+
+if __name__ == '__main__':
+    curses.update_lines_cols()
+    curses.wrapper(draw)
